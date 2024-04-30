8725ddee1b8ae964add61f88323e8bc2fc5206ea
Brigitte Bigi <contact@sppas.org>
2024-04-30 16:39:59 +0200

    Updated release version

--- whakerpy/__init__.py
+++ whakerpy/__init__.py
@@ -9,6 +9,6 @@ from .httpd import *
 from .messages import error
 from .webapp import *
 
-__version__ = "0.6"
+__version__ = "0.7"
 ___author__ = "Brigitte Bigi"
 __copyright__ = "Copyright (C) 2023-2024 Brigitte Bigi, Laboratoire Parole et Langage, Aix-en-Provence, France"
